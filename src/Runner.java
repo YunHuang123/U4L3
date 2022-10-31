f9fa2b6488198b60767f74364e5a5a9b97be485f
BT_4N2_13 <yhuang6817@bths.edu>
2022-10-31 12:32:39 -0400

    Worked on WhileLoopFun

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        WhileLoopFun fun = new WhileLoopFun();
+        System.out.println(fun.factorial(4));
+        fun.printDigits(12345);
+        System.out.println(fun.sumOfDigits(150856));
+    }
+}
